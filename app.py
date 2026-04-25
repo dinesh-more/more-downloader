d4197d03bdc7cc2e525986485cb0547cb55cf7c5
dinesh-more <moredinesh93@gmail.com>
2026-04-25 23:40:17 +0530

    updated app.py

--- app.py
+++ app.py
@@ -153,6 +153,10 @@ def download():
     return Response(generate(), mimetype='text/event-stream')
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+#     os.makedirs(DOWNLOAD_PATH, exist_ok=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
-    os.makedirs(DOWNLOAD_PATH, exist_ok=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
